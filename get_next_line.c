fb4dff257398051aa7bd20d6182d128b5df69f65
Andor Gunczer <agunczer@1-C-6.42heilbronn.de>
2021-07-16 16:12:42 +0200

    positionofn fix

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: agunczer <agunczer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/15 14:41:57 by agunczer          #+#    #+#             */
-/*   Updated: 2021/07/16 13:41:49 by agunczer         ###   ########.fr       */
+/*   Updated: 2021/07/16 16:11:11 by agunczer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,47 +37,66 @@ char	*ft_substr(char const *s, unsigned int start, unsigned int len)
 	return (ptr);
 }
 
-int     position_ofn(char *str, char c)
+int     position_ofn(char *str, int i)
 {
-    int i;
-
-    i = 0;
-    while (*str != '\0')
+    while (*(str + i) != '\0')
 	{
-		if (*str == (char)c)
+		if (*(str + i) == '\n')
 			return (i);
-		str++;
+		i++;
 	}
-	if ((char)c == '\0')
+	if (*(str + i) == '\n')
+	{
 		return (i);
+	}
 	else
+	{
 		return (0);
+	}
 }
 
-int     reader(int fd, char *warehouse)
+// static int	where_is_n(char *str, int *i)
+// {
+// 	int	j;
+
+// 	j = *i;
+// 	while (str[j] != '\0')
+// 	{
+// 		if (str[j] == '\n')
+// 		{
+// 			*i = j;
+// 			return (j);
+// 		}
+// 		j++;
+// 		if (str[j] == '\0')
+// 		{
+// 			*i = j;
+// 			return (j);
+// 		}
+// 	}
+// 	return (0);
+// }
+
+char     *reader(int fd, char *warehouse, int *readcount)
 {
-    int readcount;
     int newline;
     char *str;
     char *temp = NULL;
 
     str = malloc(BUFFER_SIZE * sizeof(char));
-    readcount = read(fd, str, BUFFER_SIZE);
-    while (readcount > 0)
+    *readcount = read(fd, str, BUFFER_SIZE);
+    while (*readcount > 0)
     {
         if (!temp)
             temp = ft_strdup("");
         temp = ft_strjoin(temp, str);
         newline = ft_strchr(str, '\n');
-        printf("newline = %d (|%s|)", newline, str);
         if (newline == 1)
             break;
         str = malloc(BUFFER_SIZE * sizeof(char));
-        readcount = read(fd, str, BUFFER_SIZE);
+        *readcount = read(fd, str, BUFFER_SIZE);
     }
-    warehouse = ft_strjoin(warehouse, temp);
-    printf("|| %s - %s - %s ||\n", warehouse, temp, str);
-    return (readcount);
+    return (ft_strjoin(warehouse, temp));
 }
 
 char    *get_next_line(int fd)
@@ -85,14 +104,17 @@ char    *get_next_line(int fd)
     static char *warehouse = NULL;
     static int i = 0;
     static int j = 0;
-    int readcount;
+    int k;
+    int readcount = 0;
 
     if (!warehouse)
         warehouse = ft_strdup("");
-    readcount = reader(fd, warehouse);
-    j = position_ofn((warehouse + i), '\n');
+    warehouse = reader(fd, warehouse, &readcount);
+    j = position_ofn(warehouse, i);
+    k = i;
     i = j + 1;
-    return (ft_substr(warehouse, i, j - i));
+    printf("k: %d - i: %d - j: %d", k, i, j); //ja da hab ich voll bock drauf
+    return (ft_substr(warehouse, k, j - k));
 }
 
 int	main(void)
@@ -103,10 +125,10 @@ int	main(void)
 	i = 0;
 	//fd = open("test.txt", O_RDONLY);
 	fd = open("test.txt", O_RDONLY);
-	while (i < 2)
+	while (i < 3)
 	{
-		printf("%s", get_next_line(fd));
-		//get_next_line(fd);
+		printf("\t-->LINE: %s\n", get_next_line(fd));
+		// get_next_line(fd);
 		i++;
 	}
 }
